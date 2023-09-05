19a076e5f2a55018034a3d6405c9ad61b61aa51b
Marcos Castro <marcos.st555@gmail.com>
2023-09-04 21:09:15 -0400

    Pagina 1

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("meu app")
 
-@app.route('/')
-def hello():
-    return "Hello World"
+posts = [
+    {"titulo": "Minha primeira postagem", "texto": "teste"},
+    {"titulo": "Segundo Post", "texto": "outro teste"},
+]
+
+@app.route("/")
+def exibir_entradas():
+    entradas = posts  # Mock das postagens
+    return render_template("exibir_entradas.html", entradas=entradas)
+
+@app.route("/layout")
+def layout ():
+    return render_template("layout.html")
 
 @app.route('/novo')
 def novo():
