396d1667a9407d4dd486a313c744445db3fc29df
Marcos Castro <marcos.st555@gmail.com>
2023-09-11 20:13:54 -0400

    Alterações aula 4

--- app.py
+++ app.py
@@ -1,21 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, abort, flash, redirect, url_for
+from posts import posts
+import sqlite3
 
-app = Flask("meu app")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pudim'
 
-posts = [
-    {"titulo": "Minha primeira postagem", "texto": "teste"},
-    {"titulo": "Segundo Post", "texto": "outro teste"},
-]
+app.config.from_object(__name__)
 
-@app.route("/")
+DATABASE = "banco.db"
+
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+@app.route('/')
 def exibir_entradas():
-    entradas = posts  # Mock das postagens
-    return render_template("exibir_entradas.html", entradas=entradas)
+    entradas = posts[::-1] # Mock das postagens
+    return render_template('exibir_entradas.html', entradas=entradas)
 
-@app.route("/layout")
-def layout ():
-    return render_template("layout.html")
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logado'] = True
+            flash("Login efetuado com sucesso!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"        
+    return render_template('login.html', erro=erro)
 
-@app.route('/novo')
-def novo():
-    return "<h1> Nova Página</h1>"
+@app.route('/logout')
+def logout():
+    session.pop('logado')
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/inserir', methods=["POST"])
+def inserir_entradas():
+    if session['logado']:
+        novo_post = {
+            "titulo": request.form['titulo'],
+            "texto": request.form['texto']
+        }
+        posts.append(novo_post)
+        flash("Post criado com sucesso!")
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/posts/<int:id>')
+def exibir_entrada(id):
+    try:
+        entrada = posts[id-1]
+        return render_template('exibir_entrada.html', entrada=entrada)
+    except Exception:
+        return abort(404)
